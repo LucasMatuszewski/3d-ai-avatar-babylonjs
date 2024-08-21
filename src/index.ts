556d505b5786de142c79f2b8bdfc203b49b74560
Łukasz Matuszewski <lucas.matuszewski@gmail.com>
2024-08-21 01:44:43 +0100

    Add Michael Figure + Fix loading .glb files in TypeScript: - Update to Babylon 7.21 and Parcel 2.12 - use new URL class constructor to load .glb files (Parcel specific) - change type to "module" in package.json and index.html - add async/await and init() function - use new loadAssetContainerAsync() from Babylon 7.21 - add Arc Rotate Camera targeted on Michael's face - use EngineFactory for optional WebGPU support (when supported by browser)

--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,70 @@
 import {
-  Engine,
   Scene,
-  FreeCamera,
   Vector3,
   HemisphericLight,
   MeshBuilder,
-  Mesh,
-  Color4,
-} from 'babylonjs';
+  loadAssetContainerAsync,
+  ArcRotateCamera,
+  EngineFactory,
+} from '@babylonjs/core';
 
-// Get the canvas DOM element
-const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
-// Load the 3D engine
-const engine = new Engine(canvas, true, {
-  preserveDrawingBuffer: true,
-  stencil: true,
-});
+import type { AbstractEngine } from '@babylonjs/core';
+
+import '@babylonjs/loaders/glTF/2.0';
+import '@babylonjs/core/Helpers/sceneHelpers';
+
+// import avatarFile from '../../../../3D Models/Michael9/Michael9-packed-resources-Blender4-BSDF-shader-WebP.glb';
+
+// Parcel 2.0 require URL constructor for unsupported file types, instead of normal import
+const avatarFile = new URL(
+  '../../../../3D Models/Michael9/Michael9-packed-resources-Blender4-BSDF-shader-WebP.glb',
+  // 'assets/Victoria9-red-dress-include-visible.glb',
+  import.meta.url
+).href;
+
+/**
+ * TODO:
+ * - optimize glb file - remove shoos, trousers, maybe remove legs or simplify mesh for legs?
+ * - add more details for the face - normal maps, HD texture? (only for face!)
+ *   - maybe I need to use Michael original skin for better quality textures? Maybe Michael HD?
+ * - find better hair?
+ * - add backed idle animation (sequence of animations) for body and face
+ * - add better lights and white background similar to Unreal Engine scene?
+ * - add animations to load from separate files but targeted to the same mesh (how to do this?)
+ * - add lip sync - how?
+ */
+
+// import forest from '../src/assets/forest.env';
+// import studioEnv from '../src/assets/Studio_Softbox_2Umbrellas_cube_specular.env';
 
 // CreateScene function that creates and return the scene
-const createScene = function () {
+const createScene = async (
+  engine: AbstractEngine,
+  canvas: HTMLCanvasElement
+) => {
   // Create a basic BJS Scene object
   const scene = new Scene(engine);
-
-  // Create a FreeCamera, and set its position to {x: 0, y: 5, z: -10}
-  const camera = new FreeCamera('camera1', new Vector3(0, 5, -10), scene);
-  // Target the camera to scene origin
-  camera.setTarget(Vector3.Zero());
-  // Attach the camera to the canvas
-  // camera.attachControl(canvas, false); // two arguments not required any more (backward compatibility)
-  camera.attachControl();
 
   // Create a basic light, aiming 0, 1, 0 - meaning, to the sky
   const light = new HemisphericLight('light1', new Vector3(0, 1, 0), scene);
 
-  // Create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation
-  // DEPRECATED:
-  //   const sphere = BABYLON.Mesh.CreateSphere(
-  //     'sphere1',
-  //     16,
-  //     2,
-  //     scene,
-  //     false,
-  //     BABYLON.Mesh.FRONTSIDE
-  //   );
-  const sphere = MeshBuilder.CreateSphere(
-    'sphere1',
-    {
-      segments: 16,
-      diameter: 2,
-      updatable: false,
-      sideOrientation: Mesh.FRONTSIDE, // default orientation (which side of mesh "face" is visible)
-    },
-    scene
-  );
+  // Import Meshes to the Scene Async: https://doc.babylonjs.com/features/featuresDeepDive/importers/loadingFileTypes#example-pg--WGZLGJ-10491
+  const assetContainer = await loadAssetContainerAsync(avatarFile, scene);
+  console.log('assetContainer', assetContainer);
+  assetContainer.addAllToScene(); // add all assets (meshes, animations, materials, textures, etc.) loaded from the file
 
-  // Move the sphere upward 1/2 of its height
-  sphere.position.y = 1;
+  const avatarRoot = assetContainer.meshes[0];
+  // make mesh 2x bigger
+  // avatarRoot.scaling = new Vector3(2, 2, 2);
+  avatarRoot.scaling.setAll(2); // shortcut instead of using Vector for each dimension
+  avatarRoot.showBoundingBox = true;
+  avatarRoot.renderOutline = true;
 
-  const box = MeshBuilder.CreateBox(
-    'box1',
-    {
-      size: 1,
-      depth: 2,
-      updatable: false,
-      faceColors: [
-        new Color4(0, 0, 1, 1), // Blue
-        new Color4(), // Black
-        new Color4(1, 0, 0, 1), // Red
-      ],
-      sideOrientation: Mesh.BACKSIDE, // only internal side of "face" visible, DOUBLESIDE = both sides of "face" visible
-    },
-    scene
-  );
+  // avatarRoot.position = new Vector3(0, 0.5, 0);
+  // avatarRoot.rotation = new Vector3(0, Math.PI, 0);
 
-  // Move the box right, upward, and front (closer)
-  box.position = new Vector3(2, 1, -2);
+  // const hairCapMesh = assetContainer.meshes[4];
+  // hairCapMesh.isVisible = false;
 
   // Create a built-in "ground" shape; its constructor takes 6 params : name, width, height, subdivision, scene, updatable
   // const ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene, false); // DEPRECATED
@@ -84,18 +74,62 @@ const createScene = function () {
     scene
   );
 
+  // Create a UniversalCamera (best for First Person games), and set its position to {x: 0, y: 5, z: -10}
+  // const camera = new UniversalCamera('camera1', new Vector3(0, 5, -10), scene);
+
+  // Create Camera rotating around the targeted point:
+  const camera = new ArcRotateCamera(
+    'camera1',
+    11, // alpha = rotate left and right
+    1.4, // beta = rotate up and down
+    1.5, // radius = zoom / distance
+    new Vector3(0, 3.15, 0), // Target the camera on the avatar's head
+    scene
+  );
+  camera.wheelPrecision = 100; // speed of scrolling (higher = slower)
+
+  // Target the camera to scene origin
+  // camera.setTarget(Vector3.Zero());
+
+  // Attach the camera to the canvas
+  // camera.attachControl(canvas, false); // two arguments not required anymore (backward compatibility)
+  camera.attachControl();
+
   // Return the created scene
   return scene;
 };
 
-// call the createScene function
-const scene = createScene();
-// run the render loop
-engine.runRenderLoop(function () {
-  scene.render();
-});
+const init = async () => {
+  // Get the canvas DOM element
+  const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
 
-// the canvas/window resize event handler (to keep same proportions of rendered scene)
-window.addEventListener('resize', function () {
-  engine.resize();
-});
+  // Engine factory use WebGPUEngine.IsSupportedAsync internally to check if WebGPU is supported
+  // then it initiates either WebGPUEngine or normal Engine (for WebGL)
+  const engine = await EngineFactory.CreateAsync(canvas, {
+    antialias: true,
+    useExactSrgbConversions: true,
+  });
+  console.log('engine.isWebGPU', engine.isWebGPU);
+
+  // Load the WebGL engine
+  // const engine = new Engine(canvas, true, {
+  //   preserveDrawingBuffer: true, // is it deprecated option?
+  //   stencil: true, // Stencil buffer texture
+  //   powerPreference: 'low-power' | 'high-performance';
+  // });
+
+  // call the createScene function
+  const scene = await createScene(engine, canvas);
+
+  // the canvas/window resize event handler (to keep same proportions of rendered scene)
+  window.addEventListener('resize', () => {
+    engine.resize();
+  });
+
+  // run the render loop
+  engine.runRenderLoop(() => {
+    scene.render();
+  });
+};
+
+init();
